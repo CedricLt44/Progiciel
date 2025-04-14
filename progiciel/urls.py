3e81d2f5bb82c3a8b129a4e9e9c33d0282bc1a49
CedricLt44 <cedric.letutour@gmail.com>
2025-04-14 21:27:14 +0200

    lancer en local du backend et front

--- progiciel/urls.py
+++ progiciel/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponseRedirect
+from django.urls import path, include
 
 
 urlpatterns = [
 
     path('progiciel-admin/', admin.site.urls),  # Nouvelle URL de l'admin
+    path('', include('backend.urls')),
 ]
